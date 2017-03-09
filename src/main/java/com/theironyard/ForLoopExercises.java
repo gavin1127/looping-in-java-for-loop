7cc432845766ba111eb7e3a644b70a0157bc1e7b
Gavin Shepherd <Gavinshepherd6688@gmail.com>
2017-03-09 13:47:26 -0500

    runs

--- src/main/java/com/theironyard/ForLoopExercises.java
+++ src/main/java/com/theironyard/ForLoopExercises.java
@@ -19,10 +19,15 @@ public class ForLoopExercises {
      * 8, 9, 10. Or, from = 12, to = -3, and step = 3, means the method will add
      * up 12, 9, 6, 3, 0, -3. The sum is returned.
      */
-    public static int addRange(int from, int to, int step){
+    public static int addRange(int from, int to, int step) {
         // todo: implement this method using a for loop
-        return 0;
+        int sum = 0;
+        for(int x = from ; x <= to ; x += step){
+            sum += x;
+        }
+        return sum;
     }
+
 
     /**
      * This method returns the specified string a given number of times. The str
@@ -32,7 +37,11 @@ public class ForLoopExercises {
      */
     public static String repeatString(String str, int times){
         // todo: implement this method
-        return null;
+         String string1="";
+        for (int x = 0; x < times; x++){
+            string1 += str;
+        }
+        return string1;
     }
 
 }
